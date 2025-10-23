1d266b4a0ca2e609bbfc29ef68c0562ac18c54a3
BlaiseKelly <blaise@kello.co.uk>
2025-10-23 10:47:58 +0200

    Added logical test incase place object not there but boundary is provided

--- R/osmactive.R
+++ R/osmactive.R
@@ -174,6 +174,11 @@ get_travel_network = function(
   ...
 ) {
   
+  # if place is empty but boundary is given
+  if(!is.null(boundary) && missing(place)) {
+    place <- boundary
+  }
+  
   osm_highways = osmextract::oe_get(place, boundary = boundary, boundary_type = boundary_type, extra_tags = extra_tags, ...)
   
   osm_highways |>
